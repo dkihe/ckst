f8eafd9a29bc92c255ac86476bbd4759dc0ab699
dkihe <dnkkihe@gmail.com>
2020-02-15 22:22:36 -1000

    Finished basic functionality of login page

---
+++ tutare/assets/login.js
@@ -0,0 +1,18 @@
+// Change later to submit form to database
+let submitForm = () => {
+    let submit = document.getElementById("submit")
+    let message = document.getElementById("message")
+    message.innerHTML = "Form Submitted"
+}
+
+// Toggle to show the password by clicking the checkbox
+let togglePassword = () => {
+    let password = document.getElementById("password")
+
+    if (password.type === "password") {
+        password.type = "text"
+    }
+    else {
+        password.type = "password"
+    }
+}
--- tutare/tutare/urls.py
+++ tutare/tutare/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about),
+    path('login/', views.login),
     path('', views.homepage),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
