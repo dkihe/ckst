2c4e90fdffa6dd2edce04b2a0d91a093c775a04c
chrisjna <35389960+chrisjna@users.noreply.github.com>
2020-02-22 05:04:36 -1000

    small UI change to login

--- tutare/assets/login.js
+++ tutare/assets/login.js
@@ -1,20 +1,20 @@
 // Change later to submit form to database
 // Submits form if all fields have been filled
 let submitForm = () => {
-    let message = document.getElementById("message")
-    let username = document.getElementById("username")
-    let password = document.getElementById("password")
+    let message = document.getElementById("message");
+    let username = document.getElementById("username");
+    let password = document.getElementById("password");
     if (username.value === "" || password.value === "") {
         message.innerHTML = "Please fill in all fields"
     }
     else {
         message.innerHTML = "Form Submitted"
     }
-}
+};
 
 // Toggle to show the password by clicking the checkbox
 let togglePassword = () => {
-    let password = document.getElementById("password")
+    let password = document.getElementById("password");
 
     if (password.type === "password") {
         password.type = "text"
@@ -22,4 +22,4 @@ let togglePassword = () => {
     else {
         password.type = "password"
     }
-}
+};
--- tutare/tutare/urls.py
+++ tutare/tutare/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about),
