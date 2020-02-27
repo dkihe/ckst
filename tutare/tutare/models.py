6f11f31fbc5a9a906e20d756155645974d29d8d8
chrisjna <35389960+chrisjna@users.noreply.github.com>
2020-02-27 03:25:46 -1000

    Added some database usage, make sure to run migrations

---
+++ tutare/tutare/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+class User(models.Model):
+    username = models.CharField(max_length=25)
+    password = models.CharField(max_length=25)
+    def __str__(self):
+        return '{} {}'.format(self.username, self.password)
