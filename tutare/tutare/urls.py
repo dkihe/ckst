f940418a1347e3895a2483588ddb9164de6290f1
Ty <tyg@hawaii.edu>
2020-03-03 20:26:02 -1000

    Setting up the User Account page

--- tutare/tutare/urls.py
+++ tutare/tutare/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about),
     path('login/', views.login),
+    path('useraccount/', views.useraccount),
     path('', views.homepage),
 ]
 
