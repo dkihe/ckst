fccdc35b53ed77a424f62cba1451527b59b4f689
chrisjna <35389960+chrisjna@users.noreply.github.com>
2020-03-15 05:49:03 -1000

    Contact Form
    Experimental need validation +  Thank you page

--- tutare/tutare/forms.py
+++ tutare/tutare/forms.py
@@ -1,6 +1,5 @@
 from django import forms
-
-from tutare.tutare.models import User
+from .models import User, ContactUs
 
 
 class UserForm(forms.ModelForm):
@@ -9,3 +8,10 @@ class UserForm(forms.ModelForm):
         widgets = {
             'password': forms.PasswordInput(),
         }
+        fields = ['password']
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = ContactUs
+        fields = ('name', 'email', 'phone', 'message')
--- tutare/tutare/models.py
+++ tutare/tutare/models.py
@@ -7,3 +7,14 @@ class User(models.Model):
 
     def __str__(self):
         return '{} / {}'.format(self.username, self.password)
+
+
+class ContactUs(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15, blank=True)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "Contact Us"
