8f447f799ff6c48aa41ebb9ff31ab56df4dd10d1
Ty <tyg@hawaii.edu>
2020-03-14 23:34:25 -1000

    Added loop to useraccount.html, Updated useraccount view

--- tutare/tutare/views.py
+++ tutare/tutare/views.py
@@ -1,19 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 def homepage(request):
     return render(request, 'homepage.html')
 
-
 def about(request):
     return render(request, 'about.html')
-
 
 def login(request):
     return render(request, 'login.html')
 
 def signup(request):
     return render(request, 'signup.html')
+
 def useraccount(request):
+    all_accounts = UserAccount.objects.all()
+    context = {
+        'all_accounts': all_accounts,
+    }
     return render(request, 'useraccount.html')
