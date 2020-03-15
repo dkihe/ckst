402dd955cff21a0d1b904c493f3b4fedacf639bb
Ty <tyg@hawaii.edu>
2020-03-15 00:41:03 -1000

    UserAccount model bug fix 4

--- tutare/tutare/migrations/0002_useraccount.py
+++ tutare/tutare/migrations/0002_useraccount.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('account', models.CharField(max_length=25)),
-                ('password', models.CharField(default='', max_length=25)),
+                ('account_password', models.CharField(default='', max_length=25)),
                 ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='tutare.User')),
             ],
         ),
