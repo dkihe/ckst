86ac8c06792cce152a98dd5f81d2c8ac06fd7a2d
chrisjna <christopherjna@gmail.com>
2020-03-14 14:29:53 -1000

    making migrations

--- tutare/tutare/views.py
+++ tutare/tutare/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def homepage(request):
     return render(request, 'homepage.html')
 
+
 def about(request):
     return render(request, 'about.html')
+
 
 def login(request):
     return render(request, 'login.html')
