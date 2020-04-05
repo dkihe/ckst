f100aa21114c4c9e1d22a4fa226f73d48b8e2cbe
Sabine Strasburger <sstrasburger451@gmail.com>
2020-04-04 15:56:49 -1000

    edited login, logout, requirements

--- tutare/tutare/views.py
+++ tutare/tutare/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
-from .forms import ContactForm
-from .models import ContactUs, UserAccount
+# from .forms import ContactForm
+# from .models import ContactUs, UserAccount
 
 
 def homepage(request):
@@ -22,35 +22,35 @@ def signup(request):
     return render(request, 'signup.html')
 
 
-def contact(request):
-    template_name = 'contact.html'
-    if request.method == "POST":
-        form = ContactForm(request.POST or None)
-        errors = None
-        if form.is_valid():
-            ContactUs.objects.create(
-                name=form.cleaned_data.get('name'),
-                email=form.cleaned_data.get('email'),
-                phone=form.cleaned_data.get('phone'),
-                message=form.cleaned_data.get('message'),
-                created_at=form.cleaned_data.get('created_at')
+# def contact(request):
+#     template_name = 'contact.html'
+#     if request.method == "POST":
+#         form = ContactForm(request.POST or None)
+#         errors = None
+#         if form.is_valid():
+#             ContactUs.objects.create(
+#                 name=form.cleaned_data.get('name'),
+#                 email=form.cleaned_data.get('email'),
+#                 phone=form.cleaned_data.get('phone'),
+#                 message=form.cleaned_data.get('message'),
+#                 created_at=form.cleaned_data.get('created_at')
+#
+#             )
+#             return HttpResponseRedirect("/")
+#         if form.errors:
+#             errors = form.errors
+#
+#         context = {"form": form, "errors": errors}
+#         return render(request, template_name, context)
+#     else:
+#         form = ContactForm()
+#         return render(request, template_name, {'form': form})
 
-            )
-            return HttpResponseRedirect("/")
-        if form.errors:
-            errors = form.errors
 
-        context = {"form": form, "errors": errors}
-        return render(request, template_name, context)
-    else:
-        form = ContactForm()
-        return render(request, template_name, {'form': form})
-
-
-def useraccount(request):
-    all_accounts = UserAccount.objects.all()
-    template = loader.get_template('useraccount.html')
-    context = {
-        'all_accounts': all_accounts,
-    }
-    return HttpResponse(template.render(context, request))
+# def useraccount(request):
+#     all_accounts = UserAccount.objects.all()
+#     template = loader.get_template('useraccount.html')
+#     context = {
+#         'all_accounts': all_accounts,
+#     }
+#     return HttpResponse(template.render(context, request))
