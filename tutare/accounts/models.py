738821737bda982797a5a4939effa453e9176661
Ty <tyg@hawaii.edu>
2020-04-11 23:01:42 -1000

    Adding the UserData model

--- tutare/accounts/models.py
+++ tutare/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class UserData(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account = models.CharField(max_length=25)
+    account_password = models.CharField(max_length=25, default='')
+
+    def __str__(self):
+        return '{} / {} / {}'.format(self.user, self.account, self.account_password)
