b4cf9acc3569d29c5c8ac9a40af4b639fb9c90d1
Ty <tyg@hawaii.edu>
2020-04-12 11:10:38 -1000

    forms.py, NewEntry form, activate Password Bank button

---
+++ tutare/accounts/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .import models
+
+class NewEntry(forms.ModelForm):
+    class Meta:
+        model = models.UserData
+        fields = ['account', 'account_password']
--- tutare/accounts/views.py
+++ tutare/accounts/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+
+from .models import UserData
+from .import forms
 
 # Create your views here.
 def signup_view(request):
