eccbba59097a6a2660c72620ae302d1954aaf317
chrisjna <35389960+chrisjna@users.noreply.github.com>
2020-04-26 17:59:42 -1000

    del function

--- tutare/accounts/urls.py
+++ tutare/accounts/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
+from .views import CleanView
 
 app_name = 'accounts'
 
@@ -10,5 +11,6 @@ urlpatterns = [
     path('login', views.login_view, name="login"),
     path('logout', views.logout_view, name="logout"),
     path('newentry', views.newentry_view, name="newentry"),
-    path('passwordbank', views.userdata_display, name="passwordbank")
+    path('passwordbank', views.userdata_display, name="passwordbank"),
+    re_path(r'^(?P<pk>\d+)/delete/', CleanView.as_view(), name ='delete')
 ]
